0121159be31d524eeb077ce3c84371eb670ce87c
Revenexant <suhridhsundaram@gmail.com>
2014-09-28 18:35:14 +0530

    Backend included now.

---
+++ InstiNow2.0/src/com/revenexant/iNow2/ComplaintActivity.java
@@ -0,0 +1,141 @@
+package com.revenexant.iNow2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class ComplaintActivity extends Activity {
+
+	private static EditText title,content;
+    private static Button submit;
+    private static List<CheckBox> tags = new ArrayList<CheckBox>();
+    JsonParser jsonparser=new JsonParser();
+    private static final String url="http://students.iitm.ac.in/mobops_testing/complaint.php";
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
+    public static int count;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_complaint);
+		title=(EditText) findViewById(R.id.editText1);
+		content=(EditText) findViewById(R.id.editText2);
+        submit=(Button) findViewById(R.id.button1);
+        tags.add(0,(CheckBox) findViewById(R.id.checkBox1));
+        tags.add(1,(CheckBox) findViewById(R.id.checkBox2));
+        tags.add(2,(CheckBox) findViewById(R.id.checkBox3));
+        tags.add(3,(CheckBox) findViewById(R.id.checkBox4));
+        tags.add(4,(CheckBox) findViewById(R.id.checkBox5));
+        tags.add(5,(CheckBox) findViewById(R.id.checkBox6));
+        tags.add(6,(CheckBox) findViewById(R.id.checkBox7));
+        tags.add(7,(CheckBox) findViewById(R.id.checkBox8));
+        tags.add(8,(CheckBox) findViewById(R.id.checkBox9));
+        
+        
+        submit.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				new CreateComplaint().execute();
+			}
+		});
+
+	}
+class CreateComplaint extends AsyncTask<String, String, String>{
+	private ProgressDialog pdialog;
+	@Override
+	protected void onPreExecute() {
+		super.onPreExecute();
+        pdialog = new ProgressDialog(ComplaintActivity.this);
+        pdialog.setMessage("Registering the complaint");
+        pdialog.setIndeterminate(false);
+        pdialog.setCancelable(true);
+        pdialog.show();
+	};
+
+	@Override
+	protected String doInBackground(String... params) {
+		int success;
+        String title1 = title.getText().toString();
+        String content1 = content.getText().toString();
+        try {
+            // Building Parameters
+        	List<BasicNameValuePair> posts = new ArrayList<BasicNameValuePair>();
+            posts.add(new BasicNameValuePair("title2", title1));
+            posts.add(new BasicNameValuePair("content2", content1));
+            for(int i=0;i<9;i++){
+            	if(tags.get(i).isChecked()){
+            		posts.add(new BasicNameValuePair("tag"+i,tags.get(i).getText().toString()));
+            		count=count+1;
+            	}
+            	else{
+            		continue;
+            	}
+            }
+            posts.add(new BasicNameValuePair("count",Integer.toString(count)));
+            //Posting user data to script 
+            JSONObject json = jsonparser.makeHttpRequest(
+                   url, "POST", posts);
+
+            Log.d("Login attempt", json.toString());
+
+            success = json.getInt(TAG_SUCCESS);
+            if (success == 1) {
+                Log.d("Complaint registered!", json.toString());                  
+                finish();
+                return json.getString(TAG_MESSAGE);
+            }else{
+                Log.d("Failure!", json.getString(TAG_MESSAGE));
+                return json.getString(TAG_MESSAGE);
+                
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+		return null;
+	}
+	@Override
+	protected void onPostExecute(String result) {
+		super.onPostExecute(result);
+		pdialog.dismiss();
+		Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+	}
+	
+}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.complaint, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
--- InstiNow2.0/src/com/revenexant/iNow2/MainActivity.java
+++ InstiNow2.0/src/com/revenexant/iNow2/MainActivity.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
@@ -13,6 +14,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -20,7 +23,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends Activity{
-    private EditText username,password;
+    private EditText username1,password1;
+    private static int test;
 	private Button login;
 	private static final String TAG_SUCCESS = "success";
     private static final String TAG_MESSAGE = "message";
@@ -32,32 +36,21 @@ public class MainActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 	        setContentView(R.layout.loginpage);
-			username=(EditText) findViewById(R.id.editText1);
-			password=(EditText) findViewById(R.id.editText2);
+			username1=(EditText) findViewById(R.id.editText1);
+			password1=(EditText) findViewById(R.id.editText2);
 			login=(Button) findViewById(R.id.button1);
 			login.setOnClickListener(new OnClickListener() {
 				
 				@Override
 				public void onClick(View v) {
-					//NetCheck();
-					
-				//please re-enable NetCheck later when logging in is possible.
-					Intent sPoint = new Intent("com.revenexant.iNow2.WINDOWPOINT");
-					startActivity(sPoint);
+					NetCheck();
 				}}
 				);}
-	 //ends this window when button is pressed.
-	 @Override
-	protected void onPause() {
-			super.onPause();
-			this.finish();
-		}
-	 
-	 //Checking for internet connection in this.
-	 private void NetCheck() 
+		    private void NetCheck() 
 	    {
 		    	if (DetectConnection
 		    		      .checkInternetConnection(MainActivity.this)) {
+		    		
 		    		     new AttemptLogin().execute();
 		    		    } else {
 		    		     Toast.makeText(MainActivity.this,
@@ -71,57 +64,83 @@ public class MainActivity extends Activity{
 			
 			
 	    }
-			class AttemptLogin extends AsyncTask<Object, Object, Object> {
+			class AttemptLogin extends AsyncTask<String, String, String> {
 		        boolean failure = false;
 				private ProgressDialog pDialog;
-		        
-		        protected void onPreExecute(String file_url) {
+		        @Override
+		        protected void onPreExecute() {
 		        	super.onPreExecute();
 		            pDialog = new ProgressDialog(MainActivity.this);
-		            pDialog.setTitle("Contacting Servers");
 		            pDialog.setMessage("Logging in ...");
 		            pDialog.setIndeterminate(false);
 		            pDialog.setCancelable(true);
 		            pDialog.show();
 		        }
 
-				@Override
-				protected Object doInBackground(Object... params) {
-					int success;
-		            String username2 = username.getText().toString();
-		            String password2= password.getText().toString();
-		            try {
-		                List<BasicNameValuePair> users = new ArrayList<BasicNameValuePair>();
-		                users.add(new BasicNameValuePair("username3", username2));
-		                users.add(new BasicNameValuePair("password3", password2));
-
-		                Log.d("request!", "starting");
-		                JSONObject json = jsonparser.makeHttpRequest(
-		                       url, "POST", users);
-		                success = json.getInt(TAG_SUCCESS);
-		                if (success == 1) {
-		                    Log.d("Login Successful!", json.toString());
-		                    
-		                    return json.getString(TAG_MESSAGE);
-		                }else{
-		                    Log.d("Login Failure!", json.getString(TAG_MESSAGE));
-		                    return json.getString(TAG_MESSAGE);      
-		                }
-		            } catch (JSONException e) {
-		                e.printStackTrace();
-		            }				return null;
-				}      
+		        @Override
+		        protected String doInBackground(String... params) {
+		        	int success;
+		        	String username2 = username1.getText().toString();
+		        	String password2= password1.getText().toString();
+		        	try {
+		        		List<BasicNameValuePair> users = new ArrayList<BasicNameValuePair>();
+		        		users.add(new BasicNameValuePair("username3", username2));
+		        		users.add(new BasicNameValuePair("password3", password2));
+		        		
+		        		Log.d("request!", "starting");
+		        		JSONObject json = jsonparser.makeHttpRequest(
+		        				url, "POST", users);
+		        		success = json.getInt(TAG_SUCCESS);
+		        		if (success == 1) {
+		        			Log.d("Login Successful!", json.toString());
+		        			test=1;
+		        			return json.getString(TAG_MESSAGE);
+		        		}else{
+		        			Log.d("Login Failure!", json.getString(TAG_MESSAGE));
+		        			return json.getString(TAG_MESSAGE);      
+		        		}
+		        	} catch (JSONException e) {
+		        		e.printStackTrace();
+		        	}				return null;
+		        }
 				protected void onPostExecute (
-						String result){
-					if(result.equals("Login successful!")){
-						Intent i = new Intent(MainActivity.this, NavigationDrawerFragment.class);
-	                    finish();
+						String file_url){
+					pDialog.dismiss();
+					if(test==1){
+						Toast.makeText(getApplicationContext(), "logged in", Toast.LENGTH_SHORT).show();
+						Intent i = new Intent(MainActivity.this, Home.class);
 	                    startActivity(i);
 					}
 					else{
-						Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+						Toast.makeText(getApplicationContext(), "Invalid Credentials" , Toast.LENGTH_LONG).show();
+						Intent i = new Intent(MainActivity.this,MainActivity.class);
+	                    finish();
+	                    startActivity(i);
 					}
+					
 				}
-		    }
 
-}
+		    }
+	    
+
+
+	    @Override
+	    public boolean onCreateOptionsMenu(Menu menu) {
+	        // Inflate the menu; this adds items to the action bar if it is present.
+	        getMenuInflater().inflate(R.menu.main, menu);
+	        return true;
+	    }
+
+	    @Override
+	    public boolean onOptionsItemSelected(MenuItem item) {
+	        // Handle action bar item clicks here. The action bar will
+	        // automatically handle clicks on the Home/Up button, so long
+	        // as you specify a parent activity in AndroidManifest.xml.
+	        int id = item.getItemId();
+	        if (id == R.id.action_settings) {
+	            return true;
+	        }
+	        return super.onOptionsItemSelected(item);
+	    }
+	}
+
