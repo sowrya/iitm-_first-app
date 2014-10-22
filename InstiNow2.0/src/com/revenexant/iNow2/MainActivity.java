5f650e0335c424b305b6fc778d2dd10fce4e36a2
Revenexant <suhridhsundaram@gmail.com>
2014-10-22 23:22:21 +0530

    Minor tweaks. Yet to solve AttemptLogin.

--- InstiNow2.0/src/com/revenexant/iNow2/MainActivity.java
+++ InstiNow2.0/src/com/revenexant/iNow2/MainActivity.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
@@ -15,7 +14,6 @@ import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -23,20 +21,18 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity{
 	private static final String sett = "mtiiops";
 	SharedPreferences save;
     private EditText username1,password1;
-    private TextView loginerror;
     private static int test;
 	private Button login;
 	private static final String TAG_SUCCESS = "success";
     private static final String TAG_MESSAGE = "message";
     public static boolean check;
-	JsonParser jsonparser=new JsonParser();
+	JsonParser jsonparser = new JsonParser();
 	 private static final String url = "http://students.iitm.ac.in/mobops_testing/login.php";
 	 
 	 @Override
@@ -49,15 +45,13 @@ public class MainActivity extends Activity{
                 startActivity(i);}
 	        }catch(Exception e){}
 	        finally{
-	        
-	        setContentView(R.layout.loginpage);
-			username1=(EditText) findViewById(R.id.editText1);
-			password1=(EditText) findViewById(R.id.editText2);
-			loginerror = (TextView) findViewById(R.id.errorText);
-			login=(Button) findViewById(R.id.button1);
-			login.setOnClickListener(new OnClickListener() {
+	        	setContentView(R.layout.loginpage);
+	        	username1=(EditText) findViewById(R.id.editText1);
+	        	password1=(EditText) findViewById(R.id.editText2);
+	        	login=(Button) findViewById(R.id.button1);
+	        	login.setOnClickListener(new OnClickListener() {
 				
-				
+	        		@Override
 				public void onClick(View v) {
 					//check connectivity
 					if(NetCheck()){
@@ -66,96 +60,87 @@ public class MainActivity extends Activity{
 					SharedPreferences.Editor editor = save.edit();
 				      editor.putBoolean("loggedin", true);
 				      editor.putString("username", username1.getText().toString());
-				      editor.commit();
-				     
-					} else {
-						//default login
-				      Intent i = new Intent(MainActivity.this, UserChoices.class);
-				      startActivity(i);}
-					
-				}}
-				);}}
+				      editor.commit();}
+				}
+			});//end OnClickListener
+	     }//end of finally
+	}//end OnCreate
 	 
 	 private boolean NetCheck() {
 		 boolean czech = false;
 		 try{
 			 czech = checkInternetConnection(MainActivity.this);
-		 }catch(Exception e){Toast.makeText(MainActivity.this,e.toString(),Toast.LENGTH_LONG).show();
+		 }catch(Exception e){Toast.makeText(MainActivity.this,"Connection check failed.",Toast.LENGTH_LONG).show();
 		 return false;}
 		 if(czech){
-		    		new AttemptLogin().execute();
-		    		return true;
+			 		try{Toast.makeText(MainActivity.this,"Logged in.",Toast.LENGTH_LONG).show();
+			 			AttemptLogin loggy = new AttemptLogin();
+			 			loggy.execute();}
+			 		catch(Exception e){
+			 			Toast.makeText(MainActivity.this,"Attempt login failed.",Toast.LENGTH_LONG).show();
+			 			czech = false;}
 		    	} else {
-		    		     Toast.makeText(MainActivity.this,
-		    		       "You Do not have Internet Connection",
-		    		       Toast.LENGTH_LONG).show();
-		    		 
-		    		     try{MainActivity.this.startActivity(new Intent(
-		    		       Settings.ACTION_WIRELESS_SETTINGS));}
-		    		     catch(Exception e){Toast.makeText(MainActivity.this,"Wireless settings fail.",Toast.LENGTH_LONG).show();}
+		    		     Toast.makeText(MainActivity.this,"No internet connection",Toast.LENGTH_LONG).show();
 		    		    }
 		 return czech;
-		    	}
-		    
-			class AttemptLogin extends AsyncTask<String, String, String> {
-		        boolean failure = false;
-				private ProgressDialog pDialog;
-		        @Override
-		        protected void onPreExecute() {
-		        	super.onPreExecute();
-		            pDialog = new ProgressDialog(MainActivity.this);
-		            pDialog.setMessage("Logging in ...");
-		            pDialog.setIndeterminate(false);
-		            pDialog.setCancelable(true);
-		            pDialog.show();
-		        }
+		   }
+	 
+	 class AttemptLogin extends AsyncTask<String, String, String> {
+			private ProgressDialog pDialog;
+	        @Override
+	        protected void onPreExecute() {
+	        	super.onPreExecute();
+	            pDialog = new ProgressDialog(MainActivity.this);
+	            pDialog.setMessage("Logging in ...");
+	            pDialog.setIndeterminate(false);
+	            pDialog.setCancelable(true);
+	            pDialog.show();
+	        }
 
-		        @Override
-		        protected String doInBackground(String... params) {
-		        	int success;
-		        	String username2 = username1.getText().toString();
-		        	String password2= password1.getText().toString();
-		        	try {
-		        		List<BasicNameValuePair> users = new ArrayList<BasicNameValuePair>();
-		        		users.add(new BasicNameValuePair("username3", username2));
-		        		users.add(new BasicNameValuePair("password3", password2));
-		        		
-		        		Log.d("request!", "starting");
-		        		JSONObject json = jsonparser.makeHttpRequest(
-		        				url, "POST", users);
-		        		success = json.getInt(TAG_SUCCESS);
-		        		if (success == 1) {
-		        			Log.d("Login Successful!", json.toString());
-		        			test=1;
-		        			return json.getString(TAG_MESSAGE);
-		        		}else{
-		        			Log.d("Login Failure!", json.getString(TAG_MESSAGE));
-		        			return json.getString(TAG_MESSAGE);      
-		        		}
-		        	} catch (JSONException e) {
-		        		e.printStackTrace();
-		        	}				return null;
-		        }
-				protected void onPostExecute (
-						String file_url){
-					pDialog.dismiss();
-					if(test==1){
-						Toast.makeText(getApplicationContext(), "logged in", Toast.LENGTH_SHORT).show();
-						Intent i = new Intent(MainActivity.this, UserChoices.class);
-	                    startActivity(i);
-					}
-					else{
-						Toast.makeText(getApplicationContext(), "Invalid Credentials" , Toast.LENGTH_LONG).show();
-						Intent i = new Intent(MainActivity.this,MainActivity.class);
-	                    finish();
-	                    startActivity(i);
-					}
-					
+	        @Override
+	        protected String doInBackground(String... params) {
+	        	int success;
+	        	String username2 = username1.getText().toString();
+	        	String password2= password1.getText().toString();
+	        	try {
+	        		List<BasicNameValuePair> users = new ArrayList<BasicNameValuePair>();
+	        		users.add(new BasicNameValuePair("username3", username2));
+	        		users.add(new BasicNameValuePair("password3", password2));
+	        		
+	        		Log.d("request!", "starting");
+	        		JSONObject json = jsonparser.makeHttpRequest(
+	        				url, "POST", users);
+	        		success = json.getInt(TAG_SUCCESS);
+	        		if (success == 1) {
+	        			Log.d("Login Successful!", json.toString());
+	        			test=1;
+	        			return json.getString(TAG_MESSAGE);
+	        		}else{
+	        			Log.d("Login Failure!", json.getString(TAG_MESSAGE));
+	        			return json.getString(TAG_MESSAGE);      
+	        		}
+	        	} catch (Exception e) {
+	        		e.printStackTrace();
+	        		Toast.makeText(getApplicationContext(), "Background error.", Toast.LENGTH_LONG).show();
+	        	}
+	        	return null;
+	        }
+			@Override
+			protected void onPostExecute (String file_url){
+				pDialog.dismiss();
+				if(test==1){
+					Toast.makeText(getApplicationContext(), "Logged in", Toast.LENGTH_LONG).show();
+					Intent i = new Intent(MainActivity.this, UserChoices.class);
+                 startActivity(i);
 				}
-
-		    }
-	    
-
+				else{
+					Toast.makeText(getApplicationContext(), "Invalid Credentials" , Toast.LENGTH_LONG).show();
+					Intent i = new Intent(MainActivity.this,MainActivity.class);
+                 startActivity(i);
+				}
+			}
+			
+	 	}// end of AttemtLogin class
 
 	    @Override
 	    public boolean onCreateOptionsMenu(Menu menu) {
@@ -185,15 +170,14 @@ public class MainActivity extends Activity{
 	    	    && con_manager.getActiveNetworkInfo().isAvailable()
 	    	    && con_manager.getActiveNetworkInfo().isConnected()) {
 	    	   return true;} else {return false;}
-	    	  
-	    	 }
-	    
-	    
-	    
+	    	}
+	   
+	    @Override
 		protected void onPause() {
 			super.onPause();
 			this.finish();
 		}
+		@Override
 		protected void onStop() {
 			super.onStop();
 		}
